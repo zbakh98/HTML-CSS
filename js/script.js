cdc32487e9c2ccb05944ef6cfcc785f043ac5f3d
zbakh98 <zbakh.ahmed98@gmail.com>
2020-10-29 15:30:35 +0100

    add js 1

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function myFunction(event) {
+    var x = event.target;
+    var z = x.parentElement.getElementsByTagName("div")[0];
+    if (z.className === "desc") {
+        show(z);
+    } else {
+        hide(z);
+    }  
+}
+
+function show(x){
+    var y = document.getElementsByClassName("show");
+    var height = getHeight(x);
+    if(y.length !== 0){
+      hide(y[0]);
+    }
+    x.style.height=height;
+    x.classList.add("show");
+
+}
+
+function hide(x){
+    x.style.height = 0;
+    x.classList.remove("show");
+}
+
+function getHeight (elem){
+    elem.style.display = 'block';
+		var height = elem.scrollHeight + 'px';
+		return height;
+}
